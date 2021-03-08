b13b63c9987f5b8008199724ef1771103b379a69
Allan McCulloch <allan@carnallan.com>
2021-03-08 00:11:37 +0000

    Add icons for green energy

---
+++ app/src/main/java/com/octoprice/android/bindingadapters/VisibilityBindingAdapter.kt
@@ -0,0 +1,9 @@
+package com.octoprice.android.bindingadapters
+
+import android.view.View
+import androidx.databinding.BindingAdapter
+
+@BindingAdapter("android:visibility")
+fun setVisibility(view: View, value: Boolean) {
+    view.visibility = if (value) View.VISIBLE else View.GONE
+}
